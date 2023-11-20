07d8730c304d8f656858cdc62df988b334dd94fd
siko0001 <tom.sikora@etudiant.univ-reims.fr>
2023-11-20 11:12:33 +0100

    Fin de séance

--- src/index.js
+++ src/index.js
@@ -19,10 +19,21 @@ print("Début");
 print("Fetch hello", 1);
 print("Fetch resources", 2);
 print("Fetch du sujet de TP", 3);
+print("Fetch hello Bob", 4);
+print("Fetch hello Bill (3)", 5);
 
 const retourHello = fetch(`${baseUrl}/resources/fetch/hello.php`);
 const retourResources = fetch(`${baseUrl}/resources/fetch/`);
 const retourSujetTP = fetch(`${baseUrl}/intranet/but/r301/tp/fetch/`);
+const retourHelloBob = fetch(`${baseUrl}/resources/fetch/hello.php?user=${encodeURIComponent('Bob')}`);
+
+const initMethode = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }// Manque le body
+};
+const retourHelloBill3 = fetch(`${baseUrl}/resources/fetch/hello.php?user=${encodeURIComponent('Bill')}&times=3`, initMethode);
 
 print("Fin");
 
@@ -41,3 +52,19 @@ retourResources.then((response) => {
 retourSujetTP.catch((response) => {
     print(`Erreur du sujet de TP`, 3)
 });
+
+retourHelloBob.then((response) => {
+    print(`Fetch hello ${responseToHtmlUl(response)}`, 4)
+    return response.text()
+})
+    .then((text) => {
+        print(text, 4)
+    });
+
+retourHelloBill3.then((response) => {
+    print(`Fetch hello ${responseToHtmlUl(response)}`, 5)
+    return response.text()
+})
+    .then((text) => {
+        print(text, 5)
+    });
