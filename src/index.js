85b90ba54fe59561de7575c8b0900089cec463c0
siko0001 <tom.sikora@etudiant.univ-reims.fr>
2023-11-27 11:11:31 +0100

    10. Abandon de requête

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-button = document.querySelector("button");
-div = document.querySelector("div.info");
-
-compteurClics = 0;
+let button = document.querySelector("button");
+let div = document.querySelector("div.info");
+let compteurClics = 0;
+let controller = null;
 
 button.addEventListener("click", () => {
+    if (controller) {
+        controller.abort();
+    }
+
+    controller = new AbortController();
+    const signal = controller.signal;
+
     compteurClics += 1;
     button.innerHTML = compteurClics;
 
-    const request = fetch(`https://iut-info.univ-reims.fr/users/jonquet/resources/fetch/echo.php?nb=${compteurClics}`);
+    const request = fetch(`https://iut-info.univ-reims.fr/users/jonquet/resources/fetch/echo.php?nb=${compteurClics}`, {signal});
     request.then((response) => response.text())
         .then((text) => {
-            div.innerText = `Last response : ${text}`;
+            div.innerText = `Last response: ${text}`;
+        })
+        .catch((error) => {
+            if (error.name === 'AbortError') {
+                console.log('Requête annulée.');
+            }
         });
 });
